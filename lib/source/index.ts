6724ffd7540f778a28ff11a40005ecbd1bd5643c
Akis2402 <kvftpjcvxn@privaterelay.appleid.com>
2026-09-19 21:32:42 +0700

    Delete lib directory

--- lib/source/index.ts
+++
@@ -1,5 +0,0 @@
-// Source processing, citation and vision extract helpers
-export function extractVision(params: any) {
-  const { VISION_EXTRACT_SYSTEM } = require('../../server/utils/visionExtract');
-  return VISION_EXTRACT_SYSTEM;
-}
